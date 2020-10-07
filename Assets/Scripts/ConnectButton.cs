4f142006c597672ee618aecb89d3187a38af75c6
kznk7 <xperia0216z@gmail.com>
2020-10-07 17:50:51 +0900

    サーバに接続するまでゲームサーバ接続ボタンを無効化

---
+++ Assets/Scripts/ConnectButton.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectButton : MonoBehaviourPunCallbacks
+{
+
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ボタンを無効に
+        button.interactable = false;
+    }
+
+    // ロビーへの接続成功
+    public override void OnJoinedLobby()
+    {
+        //ボタンを有効に
+        button.interactable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ConnectServer.cs
+++ Assets/Scripts/ConnectServer.cs
@@ -7,13 +7,14 @@ public class ConnectServer : MonoBehaviourPunCallbacks
 {
     void Start()
     {
+        // マスターサーバーに接続
         PhotonNetwork.ConnectUsingSettings();
     }
 
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
     public override void OnConnectedToMaster()
     {
-        //ロビーに接続
+        // ロビーに接続
         PhotonNetwork.JoinLobby();
     }
 
