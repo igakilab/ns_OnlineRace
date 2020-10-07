8705dc2cc827ae22dfa6ba76a4d6ac64be32fb60
kznk7 <xperia0216z@gmail.com>
2020-10-07 20:44:53 +0900

    プレイヤー画像の同期処理追加

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        var v = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        PhotonNetwork.Instantiate("Player", v, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
